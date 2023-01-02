22ccf8979b14937470ba1092d35d1b2241ed3f5a
Mikhel Róbert <robert.mikhel@gmail.com>
2023-01-02 17:55:46 +0100

    Az alap fájlok hozzáadása

---
+++ app.js
@@ -0,0 +1,9 @@
+let minusz = document.getElementById("decrease");
+    reset = document.getElementById("reset");
+    plusz = document.getElementById("increase");
+    value = document.getElementById("value").value;
+
+    plusz.addEventListener("click", function() {
+        
+        
+    })
